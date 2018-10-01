f1882e8e48500e48ae0f8faafaf8236bab771c5e
Bogdan Peleh <peleh.b97@gmail.com>
2018-10-01 18:50:47 +0300

    Add project files.

---
+++ AlgoTesterPrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTesterPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
